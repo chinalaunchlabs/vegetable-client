9f4cdcf0fedc0deb7e0f4f00501b7023f027ab0b
China Yu <china_yu@launchlabs.ph>
2016-02-24 13:15:13 +0800

    Android implementation

---
+++ Droid/Image/Bitmap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Android.Graphics;
+using System.Runtime.InteropServices;
+
+[assembly:Xamarin.Forms.Dependency(typeof(Bitmap))]
+namespace Wiggin.Drawing.Droid
+{
+	public class Bitmap: IBitmap<Android.Graphics.Bitmap>
+	{
+		const byte bytesPerPixel = 4;
+		int height; 
+		int width;
+		byte[] pixelData;
+		Android.Graphics.Bitmap bitmap;
+		string photoFile;
+
+		public Bitmap (string photo)
+		{
+			photoFile = photo;
+			var options = new BitmapFactory.Options {
+				InJustDecodeBounds = true
+			};
+
+			// Bitmap will be null because InJustDecodeBounds = true
+			bitmap = BitmapFactory.DecodeFile(photoFile, options);
+			width = options.OutWidth;
+			height = options.OutHeight;
+		}
+
+		public void ToPixelArray() {
+			bitmap = BitmapFactory.DecodeFile (photoFile);
+
+			int size = width * height * bytesPerPixel;
+			pixelData = new byte[size];
+			var byteBuffer = Java.Nio.ByteBuffer.AllocateDirect (size);
+			bitmap.CopyPixelsToBuffer (byteBuffer);
+			Marshal.Copy (byteBuffer.GetDirectBufferAddress (), pixelData, 0, size);
+			byteBuffer.Dispose ();
+		}
+
+		public void TransformImage(Func<byte, byte, byte, double> pixelOperation) {
+			byte r, g, b;
+
+			try {
+				// Pixel data order is RGB
+				for (int i = 0; i < pixelData.Length; i += bytesPerPixel) {
+					r = pixelData[i];
+					g = pixelData[i+1];
+					b = pixelData[i+2];
+
+					pixelData[i] = pixelData[i+1] = pixelData[i+2] = (byte)pixelOperation(r,g,b);
+				}
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public Android.Graphics.Bitmap ResizeImage(float maxWidth, float maxHeight) {
+			bitmap = BitmapFactory.DecodeFile (photoFile);
+
+			float maxResizeFactor = Math.Max (maxWidth / width, maxHeight / height);
+			if (maxResizeFactor > 1)
+				return bitmap;
+			int newWidth = (int)Math.Round(maxResizeFactor * width);
+			int newHeight =  (int)Math.Round(maxResizeFactor * height);
+			System.Diagnostics.Debug.WriteLine ("new size: {0}x{1}", newWidth, newHeight);
+
+			Android.Graphics.Bitmap newBitmap = Android.Graphics.Bitmap.CreateScaledBitmap (bitmap, newWidth, newHeight, false);
+			return newBitmap;
+		}
+
+		public Android.Graphics.Bitmap ToImage() {
+			var byteBuffer = Java.Nio.ByteBuffer.AllocateDirect (width * height * bytesPerPixel);
+			Marshal.Copy (pixelData, 0, byteBuffer.GetDirectBufferAddress (), width * height * bytesPerPixel);
+			bitmap.CopyPixelsFromBuffer (byteBuffer);
+			byteBuffer.Dispose ();
+			return bitmap;
+		}
+
+		public void Dispose() {
+			if (bitmap != null) {
+				bitmap.Recycle ();
+				bitmap.Dispose ();
+				bitmap = null;
+			}
+			pixelData = null;
+		}
+	}
+}
+
---
+++ Droid/Image/PhotoTransformer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Wiggin.Drawing.Droid;
+using System.Threading.Tasks;
+using System.IO;
+
+[assembly:Xamarin.Forms.Dependency(typeof(PhotoTransformer))]
+namespace Wiggin.Drawing.Droid
+{
+	public class PhotoTransformer: IPhotoTransformer
+	{
+		public PhotoTransformer ()
+		{
+		}
+
+		public Task<Stream> TransformPhotoAsync(Func<byte, byte, byte, double> pixelOperation, string imagePath) {
+			return Task.Run (() => {
+				var bitmap = new Bitmap(imagePath);
+				bitmap.ToPixelArray();
+				bitmap.TransformImage(pixelOperation);
+
+				var memoryStream = new MemoryStream();
+				var androidBitmap = bitmap.ToImage();
+				androidBitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 80, memoryStream);
+				memoryStream.Seek(0L, SeekOrigin.Begin);
+				bitmap.Dispose();
+
+				return (Stream)memoryStream;
+			});
+		}
+
+		public Task<Stream> ResizePhotoAsync(float maxWidth, float maxHeight, string imagePath) {
+			return Task.Run (() => {
+				var bitmap = new Bitmap(imagePath);
+				var memoryStream = new MemoryStream();
+				var androidBitmap = bitmap.ResizeImage(maxWidth, maxHeight);
+				androidBitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 80, memoryStream);
+				memoryStream.Seek(0L, SeekOrigin.Begin);
+				bitmap.Dispose();
+
+				return (Stream)memoryStream;
+			});
+		}
+	}
+}
+
